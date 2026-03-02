9b4ad7374109af89e71bcef91027d67bd8ca8b6e
egilibor <egilibor@gmail.com>
2026-03-02 18:18:52 +0100

    Update add_resumen_unico.py

--- add_resumen_unico.py
+++ add_resumen_unico.py
@@ -1,13 +1,4 @@
 from openpyxl import load_workbook
 
-def generar_resumen_unico(ruta_excel: str) -> None:
-    wb = load_workbook(ruta_excel)
 
-    # BORRAR SI EXISTE
-    if "RESUMEN_UNICO" in wb.sheetnames:
-        del wb["RESUMEN_UNICO"]
 
-    # INSERTAR EN POSICIÓN 1 (segundo lugar, índice 1)
-    wb.create_sheet("RESUMEN_UNICO", index=1)
-
-    wb.save(ruta_excel)
