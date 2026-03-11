ea9f45cffa3f817d24680a952737423e2dc03aca
egilibor <egilibor@gmail.com>
2026-03-11 17:51:59 +0100

    Add files via upload

---
+++ optimizar_callejero.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import requests
+
+
+def matriz_ors(coords, api_key):
+
+    url = "https://api.openrouteservice.org/v2/matrix/driving-car"
+
+    headers = {
+        "Authorization": api_key,
+        "Content-Type": "application/json"
+    }
+
+    body = {
+        "locations": [[lon, lat] for lat, lon in coords]
+    }
+
+    r = requests.post(url, json=body, headers=headers)
+
+    return r.json()["durations"]
+
+
+def optimizar_rutas_callejero(input_excel, output_excel, api_key):
+
+    xls = pd.ExcelFile(input_excel)
+    writer = pd.ExcelWriter(output_excel)
+
+    for sheet in xls.sheet_names:
+
+        df = pd.read_excel(xls, sheet)
+
+        if "Latitud" not in df.columns:
+            df.to_excel(writer, sheet_name=sheet, index=False)
+            continue
+
+        coords = list(zip(df["Latitud"], df["Longitud"]))
+
+        matriz = matriz_ors(coords, api_key)
+
+        # orden simple inicial
+        orden = sorted(range(len(coords)), key=lambda i: matriz[0][i])
+
+        df = df.iloc[orden]
+
+        df.to_excel(writer, sheet_name=sheet, index=False)
+
+    writer.close()
